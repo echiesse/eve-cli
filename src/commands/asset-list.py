4f752124d0729e1ea24101eea35f28e4c3ab7cbf
Eric Chiesse <echiesse@gmail.com>
2026-06-30 20:54:17 -0300

    Initial implementation of item name retrieval using the ESI. It needs to be changed to use the SDE instead.

--- src/commands/asset-list.py
+++ src/commands/asset-list.py
@@ -8,6 +8,19 @@ CHARACTER_ID = '2117307808'
 
 #URL = 'https://esi.evetech.net/characters/{character_id}/assets'
 
+'''
+ {'is_singleton': True,
+  'item_id': 1035415566648,
+  'location_flag': 'Unlocked',
+  'location_id': 1035489010147,
+  'location_type': 'item',
+  'quantity': 1,
+  'type_id': 31154}
+'''
+
 def run(characterId):
     data = tranquility.getCharacterInventory(characterId)
-    pprint.pprint(data)
+    print('Name                                         | Quantity')
+    for item in data[:10]:
+        item_detail = tranquility.getItem(item['type_id'])
+        print(f'{item_detail["name"]} | {item["quantity"]}')
