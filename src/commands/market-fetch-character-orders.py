f314409c74abdb2e49d9a2a3bb78e1c60d0d9169
Eric Chiesse <echiesse@gmail.com>
2026-08-25 23:00:47 -0300

    Saving market orders in the .evecli dir in the user home dir.

--- src/commands/market-fetch-character-orders.py
+++ src/commands/market-fetch-character-orders.py
@@ -5,7 +5,7 @@ from application.factories import sdeManagerFromConfig
 from base.inventory import buildInventory, getItemNames, printInventory
 from base import eveClient
 from base.inventory import filterLeaf
-from support.utils import jprint, saveJson
+from support.utils import ensureDir, jprint, saveJson, showDateTime
 
 import config
 
@@ -15,11 +15,8 @@ CHARACTER_ID = config.CHARACTER_ID_AERO_SCRIPTER
 tranquility = eveClient.DataSource(eveClient.ServerNames.TRANQUILITY)
 sde = sdeManagerFromConfig()
 
-HOME_DIR = '..' # TODO: Use the user home in production
-#HOME_DIR = os.path.expanduser('~/.evecli') # TODO: Use the user home in production
-
 MARKET_ORDERS_FILE_NAME = 'market-orders.json'
-MARKET_ORDERS_FILE = os.path.join(HOME_DIR, MARKET_ORDERS_FILE_NAME)
+MARKET_ORDERS_FILE = os.path.join(config.EVECLI_DIR, MARKET_ORDERS_FILE_NAME)
 
 '''
 id, type_name, quantity, current_cost
@@ -27,8 +24,13 @@ id, type_name, quantity, current_cost
 
 JITA_4_4_STATION_ID = '60003760'
 
-def run(characterId):
-    orders = tranquility.getCharacterOrders(characterId, int(JITA_4_4_STATION_ID))
-    saveJson(orders, MARKET_ORDERS_FILE, indent = 2)
-    #orders = tranquility.getCharacterOrders(characterId)
+def run(characterId, locationId):
+
+    characterDataDir = os.path.join(config.EVECLI_DIR, 'data', characterId)
+    ensureDir(characterDataDir)
+    ordersFilename = f'market_orders-{locationId}-{showDateTime()}.json'
+    ordersFilePath = os.path.join(characterDataDir, ordersFilename)
+
+    orders = tranquility.getCharacterOrders(characterId, int(locationId))
+    saveJson(orders, ordersFilePath, indent = 2)
     jprint(orders)
